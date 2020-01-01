d55b303ba7b367e56ff29c1812bd304e20b28f68
DP3PO <williampferebee@gmail.com>
2020-01-01 18:29:15 -0500

    Updates

---
+++ precache-manifest.7d3cb9abf78dbfbb047352a18402cf25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dfece504894330d06d1d9a24f1fb692",
+    "url": "/React_Movie-Search/index.html"
+  },
+  {
+    "revision": "3126c95475c24bedec06",
+    "url": "/React_Movie-Search/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "87b4614a8ab5eda00353",
+    "url": "/React_Movie-Search/static/css/main.e99a2b8f.chunk.css"
+  },
+  {
+    "revision": "3126c95475c24bedec06",
+    "url": "/React_Movie-Search/static/js/2.2e88f341.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/React_Movie-Search/static/js/2.2e88f341.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87b4614a8ab5eda00353",
+    "url": "/React_Movie-Search/static/js/main.9c8fcedd.chunk.js"
+  },
+  {
+    "revision": "57dd6039b52ace404ac9",
+    "url": "/React_Movie-Search/static/js/runtime-main.4ce4e7b9.js"
+  }
+]);
